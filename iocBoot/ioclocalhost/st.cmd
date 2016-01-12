eb135e783d93147b1ee3a7596ac983182d62f63f
Daron Chabot <dchabot@bnl.gov>
2016-01-12 18:01:23 -0500

    ENH: add motorsim support

--- iocBoot/ioclocalhost/st.cmd
+++ iocBoot/ioclocalhost/st.cmd
@@ -1,18 +1,47 @@
 #!../../bin/linux-x86_64/simioc
 
-## You may have to change simioc to something else
+## You may have to change motorSim to something else
 ## everywhere it appears in this file
 
-#< envPaths
+< envPaths
+
+epicsEnvSet('EPICS_CA_AUTO_ADDR_LIST', 'NO')          
+epicsEnvSet('EPICS_CA_ADDR_LIST', '127.0.0.1')
+
+cd ${TOP}
 
 ## Register all support components
-dbLoadDatabase("../../dbd/simioc.dbd",0,0)
+dbLoadDatabase("dbd/simioc.dbd",0,0)
 simioc_registerRecordDeviceDriver(pdbbase) 
 
 ## Load record instances
-dbLoadRecords("../../db/simioc.db","user=dchabot")
+dbLoadTemplate("db/sensor.substitutions")
+dbLoadTemplate("db/motorSim.substitutions")
+dbLoadRecords("db/fakemotor.db", "Sys=XF:31IDA-OP,Dev={Tbl-Ax:FakeMtr},Mtr=XF:31IDA-OP{Tbl-Ax:X1}Mtr")
 
+# Create simulated motors: ( start card , start axis , low limit, high limit, home posn, # cards, # axes to setup)
+motorSimCreate( 0, 0, -32000, 32000, 0, 1, 6 )
+# Setup the Asyn layer (portname, low-level driver drvet name, card, number of axes on card)
+drvAsynMotorConfigure("motorSim1", "motorSim", 0, 6)
+
+## autosave/restore machinery
+save_restoreSet_Debug(0)
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+
+set_savefile_path("${TOP}/as","/save")
+set_requestfile_path("${TOP}/as","/req")
+
+set_pass0_restoreFile("info_positions.sav")
+set_pass0_restoreFile("info_settings.sav")
+set_pass1_restoreFile("info_settings.sav")
+
+cd ${TOP}/iocBoot/${IOC}
 iocInit()
 
-## Start any sequence programs
-#seq sncsimioc,"user=dchabot"
+## more autosave/restore machinery
+cd ${TOP}/as/req
+makeAutosaveFiles()
+create_monitor_set("info_positions.req", 5 , "")
+create_monitor_set("info_settings.req", 15 , "")
+
